d1f500090b3578d5567bb4fe967079fcc470d8e9
Swati Manjari Panda <swatipanda2324@gmail.com>
2026-05-10 00:19:32 +0530

    Update index.js

--- index.js
+++ index.js
@@ -13,51 +13,51 @@ document.querySelectorAll("button")[i].addEventListener("click",function()
 function MakeSound(key){
     switch (key) {
         case "cat1":
-            var cat1=new Audio("./sounds/aagh.mp3");
+            var cat1=new Audio("./aagh.mp3");
             cat1.play();
             break;
         case "cat2":
-            var cat2=new Audio("./sounds/cool.mp3");
+            var cat2=new Audio("./cool.mp3");
             cat2.play();
             break;
         case "cat3":
-            var cat3=new Audio("./sounds/doggy.mp3");
+            var cat3=new Audio("./doggy.mp3");
             cat3.play();
             break;
         case "cat4":
-            var cat4=new Audio("./sounds/fahh.mp3");
+            var cat4=new Audio("./fahh.mp3");
             cat4.play();
             break;
         case "cat5":
-            var cat5=new Audio("./sounds/freaky.mp3");
+            var cat5=new Audio("./freaky.mp3");
             cat5.play();
             break;
         case "cat6":
-            var cat6=new Audio("./sounds/huh.mp3");
+            var cat6=new Audio("./huh.mp3");
             cat6.play();
             break;
         case "cat7":
-            var cat7=new Audio("./sounds/laugh.mp3");
+            var cat7=new Audio("./laugh.mp3");
             cat7.play();
             break;
         case "cat8":
-            var cat8=new Audio("./sounds/oioiioiioioioi.mp3");
+            var cat8=new Audio("./oioiioiioioioi.mp3");
             cat8.play();
             break;
         case "cat9":
-            var cat9=new Audio("./sounds/sad.mp3");
+            var cat9=new Audio("./sad.mp3");
             cat9.play();
             break;
         case "cat10":
-            var cat10=new Audio("./sounds/sleepy.mp3");
+            var cat10=new Audio("./sleepy.mp3");
             cat10.play();
             break;
         case "cat11":
-            var cat11=new Audio("./sounds/sus.mp3");
+            var cat11=new Audio("./sus.mp3");
             cat11.play();
             break;
         case "cat12":
-            var cat12=new Audio("./sounds/war-sound.mp3");
+            var cat12=new Audio("./war-sound.mp3");
             cat12.play();
             break;
         default:
